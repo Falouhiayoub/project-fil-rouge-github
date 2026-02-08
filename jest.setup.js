6ea8f1b1a97fe5274007ad9e5edd02e910e893fc
Falouhiayoub <falouhiayoub9@gmail.com>
2026-02-08 14:53:17 +0100

    change the setup for testing

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,13 @@
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+jest.mock('./src/config/env', () => ({
+    VITE_N8N_WEBHOOK_URL: 'http://mock-webhook.com',
+    VITE_N8N_WEBHOOK_URL_CONTACT_PAGE: 'http://mock-contact-webhook.com',
+    VITE_GEMINI_API_KEY: 'mock-api-key',
+    VITE_CLOUDINARY_CLOUD_NAME: 'mock-cloud',
+    VITE_CLOUDINARY_UPLOAD_PRESET: 'mock-preset'
+}));
