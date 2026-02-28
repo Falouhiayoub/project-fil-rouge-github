71d7f833d11b79c9f511cb8478e562ed81680160
Falouhiayoub <falouhiayoub9@gmail.com>
2026-02-28 15:19:03 +0000

    add the google cloud service for the google authentication

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { store } from './redux/store';
 import App from './App';
+import { VITE_GOOGLE_CLIENT_ID } from './config/env';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <HelmetProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <GoogleOAuthProvider clientId={VITE_GOOGLE_CLIENT_ID}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </GoogleOAuthProvider>
       </HelmetProvider>
     </Provider>
   </React.StrictMode>,
